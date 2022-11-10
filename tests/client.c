cc6b76e14efc42c66bd59371872de1e59a2786ef
ChenZhigang <hellochenzg@163.com>
2022-11-10 10:33:07 -0500

    update [^] testing about readline

--- tests/client.c
+++ tests/client.c
@@ -2,25 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <unistd.h>
-#include <termios.h>
+#include <readline/readline.h>
 
 int main(int argc, char *argv[])
 {
-	char command[128];
-
 	while (1)
 	{
-		memset(command, 0, 128);
-		printf("cli > ");
-		if (fgets(command, 128, stdin) != NULL)
-		{
-			if (strcmp(command, "exit\n") == 0)
-				break;
-			if (strcmp(command, "\n") == 0)
-				continue;
-			printf("do something ... %s", command);
-		}
+		char *input = readline("olapcli > ");
+
+		if (*input == 0)
+			continue;
+
+		if (strcmp(input, "exit") == 0)
+			break;
+
+		printf("do something ... %s\n", input);
+
+		free(input);
 	}
 
 	return 0;
