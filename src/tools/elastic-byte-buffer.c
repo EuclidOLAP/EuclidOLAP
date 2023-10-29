7e31ab13af908a3b842b3c344be1d9eabc774f2e
hfyf <czg@euclidolap.com>
2023-10-29 12:17:48 -0400

    formatted: src/tools/elastic-byte-buffer.c

--- src/tools/elastic-byte-buffer.c
+++ src/tools/elastic-byte-buffer.c
@@ -15,7 +15,8 @@
 // #include "rb-tree.h"
 #include "elastic-byte-buffer.h"
 
-ByteBuf *buf__alloc(size_t capacity) {
+ByteBuf *buf__alloc(size_t capacity)
+{
 	assert(capacity > 0);
 
 	ByteBuf *byte_buf = obj_alloc(sizeof(ByteBuf), OBJ_TYPE__ByteBuf);
@@ -26,18 +27,22 @@ ByteBuf *buf__alloc(size_t capacity) {
 	return byte_buf;
 }
 
-void buf_clear(ByteBuf *byte_buf) {
+void buf_clear(ByteBuf *byte_buf)
+{
 	byte_buf->index = 0;
 }
 
-void buf_release(ByteBuf *byte_buf) {
+void buf_release(ByteBuf *byte_buf)
+{
 	// obj_release(byte_buf->buf_addr);
 	release_memory(byte_buf->buf_addr);
 	obj_release(byte_buf);
 }
 
-void *buf_cutting(ByteBuf *byte_buf, size_t count) {
-	if (byte_buf->capacity - byte_buf->index < count) {
+void *buf_cutting(ByteBuf *byte_buf, size_t count)
+{
+	if (byte_buf->capacity - byte_buf->index < count)
+	{
 		size_t added = count + byte_buf->index - byte_buf->capacity + 2048;
 		byte_buf->capacity += added;
 		byte_buf->buf_addr = realloc(byte_buf->buf_addr, byte_buf->capacity);
@@ -47,34 +52,42 @@ void *buf_cutting(ByteBuf *byte_buf, size_t count) {
 	return next_addr;
 }
 
-char *buf_cursor(ByteBuf *byte_buf) {
+char *buf_cursor(ByteBuf *byte_buf)
+{
 	return buf_cutting(byte_buf, 0);
 }
 
-char *buf_starting(ByteBuf *byte_buf) {
+char *buf_starting(ByteBuf *byte_buf)
+{
 	return byte_buf->buf_addr;
 }
 
-size_t buf_size(ByteBuf *byte_buf) {
+size_t buf_size(ByteBuf *byte_buf)
+{
 	return byte_buf->index;
 }
 
-void buf_mv_cursor(ByteBuf *buf, ssize_t offset) {
+void buf_mv_cursor(ByteBuf *buf, ssize_t offset)
+{
 	buf->index += offset;
 }
 
-void buf_set_cursor(ByteBuf *buf, size_t position) {
+void buf_set_cursor(ByteBuf *buf, size_t position)
+{
 	buf->index = position;
 }
 
-void buf_append_short(ByteBuf *buf, short sval) {
+void buf_append_short(ByteBuf *buf, short sval)
+{
 	*((short *)buf_cutting(buf, sizeof(short))) = sval;
 }
 
-void buf_append_int(ByteBuf *buf, int ival) {
+void buf_append_int(ByteBuf *buf, int ival)
+{
 	*((int *)buf_cutting(buf, sizeof(int))) = ival;
 }
 
-void buf_append_long(ByteBuf *buf, long lval) {
+void buf_append_long(ByteBuf *buf, long lval)
+{
 	*((long *)buf_cutting(buf, sizeof(long))) = lval;
 }
