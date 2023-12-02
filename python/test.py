1297189c0b6da5a12f71d810ca4f8e813eec3941
ChenZhigang <chenzg@tiens.com>
2023-12-02 09:38:27 +0800

    ^

--- python/test.py
+++ python/test.py
@@ -12,7 +12,7 @@ def create_standard_demo_metadata():
     olap_cli.close()
 
 
-create_standard_demo_metadata()
+# create_standard_demo_metadata()
 
 
 def test_01():
@@ -20,6 +20,16 @@ def test_01():
     print(len(a_str))
     a_str += "\0"
     print(len(a_str))
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    tuple_a = OlapClient("192.168.66.66", 8760)  # (1, 22, 333)
+    tuple_b = OlapClient("192.168.66.66", 8760)  # (1, 22, 333)
+    tuple_c = tuple_b
+    print(tuple_a == tuple_b)
+    print(tuple_a is tuple_b)
+    print(tuple_c is tuple_b)
+    print(id(tuple_a))
+    print(id(tuple_b))
+    print(id(tuple_c))
 
 
-# test_01()
+test_01()
