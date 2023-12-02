4d6849f12ee83a7b310a69166c6748350c5fc59e
ChenZhigang <chenzg@tiens.com>
2023-12-02 15:15:22 +0800

    updated test.py

--- python/test.py
+++ python/test.py
@@ -12,7 +12,35 @@ def create_standard_demo_metadata():
     olap_cli.close()
 
 
-# create_standard_demo_metadata()
+def query_mdx():
+    olap_cli = OlapClient("192.168.66.66", 8760)
+    olap_cli.connect()
+    olap_cli.execute("""with
+  member [Measures].SSSSSS as
+    sum({ ([Measures].XXX), ([Measures].YYY), ([Measures].ZZZ) })
+  member [Measures].XXX
+    as ([Measures].[sales count]) * 10
+  member [Measures].YYY
+    as (([Measures].XXX) + 111)
+  member [Measures].ZZZ
+    as ([Measures].YYY) / 8 - 222.22
+  member [Measures].QQQ
+    as sum({([Measures].[sales count]), ([Measures].[sales])})
+  member Dates.VVV
+    as sum({ ([Dates].[ALL].[2022]), ([Dates].[ALL].[2023]) })
+  member [Measures].IIIIII
+    as IIF( ([Measures].SSSSSS) > 2000000, 200, 404 )
+  member [Measures].FFFFFF
+    as IIF( ([Measures].IIIIII) = 200, 321, 900000000 )
+select
+  { ([Measures].SSSSSS), ([Measures].IIIIII), ([Measures].FFFFFF) } on 0,
+  filter(
+    members(Dates),
+    ((([Measures].SSSSSS) <= 30000)or (([Measures].SSSSSS) >= 40000))
+      and ((([Measures].SSSSSS) <= 360000) or (([Measures].SSSSSS) >= 370000))
+  ) on 1
+from [Andes Online Store];""")
+    olap_cli.close()
 
 
 def test_01():
@@ -32,4 +60,10 @@ def test_01():
     print(id(tuple_c))
 
 
-test_01()
+def main():
+    # test_01()
+    # create_standard_demo_metadata()
+    query_mdx()
+
+
+main()
